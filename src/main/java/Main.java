fe1934b02ab1ad47c797cb827b3a4ac30c0678ed
User, 2 <User2@att.com>
2018-08-09 10:24:06 +0300

    commit 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main{
 	public static void main(String[] args){
 		//System.out.println("change1");
 		//System.out.println("change2");
-		//System.out.println("change3");
+		//System.out.println("change3");///////
 		System.out.println("Hello World");
 		
 	}
